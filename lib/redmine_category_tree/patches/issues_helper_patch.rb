b2e321738850fcf0d97bc217642b3bcefb635821
apsmir <ap.smir@gmail.com>
2024-09-16 19:56:44 +0300

    bug fix - history show incorrect journal details

--- lib/redmine_category_tree/patches/issues_helper_patch.rb
+++ lib/redmine_category_tree/patches/issues_helper_patch.rb
@@ -8,24 +8,24 @@ module RedmineCategoryTree
 			end
 
 			def find_name_by_reflection(field, id)
-				unless id.present?
-					return nil
-				end
-				if field == 'category'
-					@detail_value_name_by_reflection ||= Hash.new do |hash, key|
-						association = Issue.reflect_on_association(field.to_sym)
-						if association
-							record = association.klass.find_by_id(id)
-							if record
-								Rails.logger.info "  - Yes, generating tree inline"
-								hash[key] = render_issue_category_with_tree_inline(record).force_encoding('UTF-8')
+				return nil if id.blank?
+
+				@detail_value_name_by_reflection ||= Hash.new do |hash, key|
+					association = Issue.reflect_on_association(key.first.to_sym)
+					name = nil
+					if association
+						record = association.klass.find_by_id(key.last)
+						if (record && !record.is_a?(Project)) || (record.is_a?(Project) && record.visible?)
+							if key.first == 'category'
+								name = render_issue_category_with_tree_inline(record).force_encoding('UTF-8')
+							else
+								name = record.name.force_encoding('UTF-8')
 							end
 						end
 					end
-					super(field, id) unless @detail_value_name_by_reflection[[field, id]]
-				else
-					super(field, id)
+					hash[key] = name
 				end
+				@detail_value_name_by_reflection[[field, id]]
 			end
 		end
 	end
